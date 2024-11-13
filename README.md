7c74a9def9ab1be93dd4c74bed2f90812bdc0a9b
ian_reginald <reginaldian7@gmail.com>
2024-11-13 16:13:20 +0300

    j.s assignment

--- README.md
+++
@@ -1,1 +0,0 @@
-# js_basics_lab
---
+++ app.js
@@ -0,0 +1,9 @@
+const b= 1+3;
+console.log(b);
+
+let myarray=[{name:'John',age:25,colour:'red'},
+             {name:'Mark',age:22,colour:'blue'},
+             {name:'Mercy',age:25,colour:'purple'},
+             {name:'Olivia',age:20,colour:'orange'}];
+
+console.log(myarray);
